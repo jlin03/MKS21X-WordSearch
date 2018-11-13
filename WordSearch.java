9ca0c364e85f41b32f92e73b062f0e6770678172
jlin03 <jlin03@stuy.edu>
2018-11-13 10:09:15 -0500

    preliminary code for addallwords written

--- WordSearch.java
+++ WordSearch.java
@@ -21,11 +21,11 @@ public class WordSearch {
 		catch(FileNotFoundException e) {
 			System.out.println("File not found: " + fileName);
 		}
-	
+
 		seed = (int)System.currentTimeMillis();
 		randgen = new Random(seed);
 	}
-	
+
 	public WordSearch(int rows, int cols, String fileName, int randSeed) {
 		data = new char[rows][cols];
 		clear();
@@ -39,21 +39,21 @@ public class WordSearch {
 		catch(FileNotFoundException e) {
 			System.out.println("File not found: " + fileName);
 		}
-		
+
 		seed = randSeed;
-		randgen = new Random(seed);	
+		randgen = new Random(seed);
 	}
-	
+
 	private void clear() {
 		for(int r = 0;r < data.length;r++) {
 			for(int c = 0;c < data[0].length;c++) {
 				data[r][c] = '_';
 			}
 		}
-	}	
-	
+	}
+
 /**Attempts to add a given word to the specified position of the WordGrid.
-     *The word is added in the direction rowIncrement,colIncrement 
+     *The word is added in the direction rowIncrement,colIncrement
 
      *Words must have a corresponding letter to match any letters that it overlaps.
 
@@ -65,16 +65,16 @@ public class WordSearch {
      *@param rowIncrement is -1,0, or 1 and represents the displacement of each letter in the row direction
      *@param colIncrement is -1,0, or 1 and represents the displacement of each letter in the col direction
 
-     *@return true when: the word is added successfully. 
+     *@return true when: the word is added successfully.
      *        false when: the word doesn't fit, OR  rowchange and colchange are both 0,
      *        OR there are overlapping letters that do not match
      */
-	
+
 	private boolean addWord(String word,int row, int col, int rowIncrement, int colIncrement){
 		if(rowIncrement == 0 && colIncrement == 0) {
 			return false;
 		}
-	
+
 		//if the word breaks past the boundaries of the array index limits
 		if((row + (rowIncrement * word.length()) - 1) >= data[0].length || (row + (rowIncrement * word.length()) - 1) < 0) {
 			return false;
@@ -82,29 +82,47 @@ public class WordSearch {
 		if((col + (colIncrement * word.length()) - 1) >= data.length || (col + (colIncrement * word.length()) - 1) < 0) {
 			return false;
 		}
-		
+
 		//check if all characters match
 		for(int i = 0;i < word.length();i++) {
 			if(data[row+(i * rowIncrement)][col+(i * colIncrement)] != word.charAt(i) && data[row+(i * rowIncrement)][col+(i * colIncrement)] != '_') {
 				return false;
 			}
 		}
-		
+
 		//add the word in
 		for(int i = 0;i < word.length();i++) {
 			data[row+(i * rowIncrement)][col+(i * colIncrement)] = word.charAt(i);
 		}
 		return true;
-		
-		
-		
     }
-	
-	
-	
-	
-	
-	
+
+		private void addAllWords() {
+			int wordIndex;
+			int rowInc;
+			int colInc;
+			int r;
+			int c;
+			int tries = 50;
+			while(wordsToAdd.size() != 0) {
+				wordIndex = randgen.nextInt() % wordsToAdd.size();
+				rowInc = randgen.nextInt() % 100;
+
+
+
+			}
+
+
+
+
+		}
+
+
+
+
+
+
+
 	public String toString() {
 		String out = "";
 		//puzzle
@@ -116,7 +134,7 @@ public class WordSearch {
 			out += data[r][data[0].length - 1];
 			out += "|\n";
 		}
-		
+
 		//words
 		out += "Words: ";
 		if(wordsAdded.size() > 0) {
@@ -126,10 +144,9 @@ public class WordSearch {
 			out += wordsAdded.get(wordsAdded.size() - 1);
 		}
 		out += "(seed: " + seed + ")";
-		
+
 		return out;
 	}
-	
+
 
 }
-
